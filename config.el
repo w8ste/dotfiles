224d50be9ee20db1ea09e18d385154f4ecd0a211
Hannes Albert <hannes10042004@icloud.com>
2023-09-08 17:09:45 +0200

    Switched from linum to built in lines

--- config.el
+++ config.el
@@ -135,9 +135,29 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(use-package linum-relative
-  :ensure t
-  :after (evil)
-  :config
-  (add-hook 'prog-mode-hook 'linum-on)
-  (linum-relative-global-mode))
+;;(use-package linum-relative
+    ;;:ensure t
+    ;;:after (evil)
+    ;;:config
+    ;;(add-hook 'prog-mode-hook 'linum-on)
+    ;;(linum-relative-global-mode))
+  (global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)l
+     (global-visual-line-mode t)
+
+(use-package which-key
+:init
+  (which-key-mode 1)
+:config
+(setq which-key-side-window-location 'bottom
+	which-key-sort-order #'which-key-key-order-alpha
+	which-key-sort-uppercase-first nil
+	which-key-add-column-padding 1
+	which-key-max-display-columns nil
+	which-key-min-display-lines 6
+	which-key-side-window-slot -10
+	which-key-side-window-max-height 0.25
+	which-key-idle-delay 0.8
+	which-key-max-description-length 25
+	which-key-allow-imprecise-window-fit t
+	which-key-separator " → " ))
