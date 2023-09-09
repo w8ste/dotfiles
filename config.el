c49fd8cd8b1ced0f99ecbc41b07394b9d0c30547
Hannes Albert <hannes10042004@icloud.com>
2023-09-09 18:37:27 +0200

    Added discord support and minor update

--- config.el
+++ config.el
@@ -3,9 +3,9 @@
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-			      :ref nil
-			      :files (:defaults (:exclude "extensions"))
-			      :build (:not elpaca--activate-package)))
+                              :ref nil
+                              :files (:defaults (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
@@ -15,18 +15,18 @@
     (make-directory repo t)
     (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
-	(if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-		 ((zerop (call-process "git" nil buffer t "clone"
-				       (plist-get order :repo) repo)))
-		 ((zerop (call-process "git" nil buffer t "checkout"
-				       (or (plist-get order :ref) "--"))))
-		 (emacs (concat invocation-directory invocation-name))
-		 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-				       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-		 ((require 'elpaca))
-		 ((elpaca-generate-autoloads "elpaca" repo)))
-	    (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-	  (error "%s" (with-current-buffer buffer (buffer-string))))
+        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                 ((zerop (call-process "git" nil buffer t "clone"
+                                       (plist-get order :repo) repo)))
+                 ((zerop (call-process "git" nil buffer t "checkout"
+                                       (or (plist-get order :ref) "--"))))
+                 (emacs (concat invocation-directory invocation-name))
+                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                 ((require 'elpaca))
+                 ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
@@ -34,12 +34,11 @@
     (load "./elpaca-autoloads")))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
-
 ;; Install use-package support
-  (elpaca elpaca-use-package
-    ;; Enable :elpaca use-package keyword.
-    (elpaca-use-package-mode)
-    ;; Assume :elpaca t unless otherwise specified.
+(elpaca elpaca-use-package
+  ;; Enable :elpaca use-package keyword.
+  (elpaca-use-package-mode)
+  ;; Assume :elpaca t unless otherwise specified.
     (setq elpaca-use-package-by-default t))
 
   ;; Block until current queue processed.
@@ -51,56 +50,83 @@
   ;;For example:
   ;;(USE-package general :demand t)
   ;;(elpaca-wait)
-  (use-package evil
-    :init
-    (setq evil-want-integration t)
-    (setq evil-want-keybinding nil)
-    (setq evil-vsplit-window-right t)
-    (setq evil-split-window-below t)
-    (evil-mode))
-  (use-package evil-collection
-    :after evil
-    :config
-    (setq evil-collection-mode-list '(dashboard dired ibuffer))
-    (evil-collection-init))
-
-  (with-eval-after-load 'evil-maps
-  (define-key evil-motion-state-map (kbd "SPC") nil)
-  (define-key evil-motion-state-map (kbd "RET") nil)
-  (define-key evil-visual-state-map (kbd "C-c") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
-  (define-key evil-motion-state-map (kbd "C-e") nil)
-  (define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state)
-  (define-key evil-motion-state-map (kbd "TAB") nil))
-;; Setting RETURN key in org-mode to follow links
-  (setq org-return-follows-link  t)
-
   ;;Turns off elpaca-use-package-mode current declartion
   ;;Note this will cause the declaration to be interpreted immediately (not deferred).
   ;;Useful for configuring built-in emacs features.
   (use-package emacs :elpaca nil :config (setq ring-bell-function #'ignore))
 
   ;; Don't install anything. Defer execution of BODY
-  (elpaca nil (message "deferred"))
+  ;;(elpaca nil (message "deferred"))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (evil-mode))
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-collection-mode-list '(dashboard dired ibuffer))
+  (evil-collection-init))
+
+;; settings keybindings for evil
+(with-eval-after-load 'evil-maps
+(define-key evil-motion-state-map (kbd "SPC") nil)
+(define-key evil-motion-state-map (kbd "RET") nil)
+(define-key evil-visual-state-map (kbd "C-c") 'evil-normal-state)
+(define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
+(define-key evil-motion-state-map (kbd "C-e") nil)
+(define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state)
+(define-key evil-motion-state-map (kbd "TAB") nil))
 
 (use-package general
-      :config
-      (general-evil-setup)
-       ;; set up 'SPC' as the global leader key
-    (general-create-definer w8ste/leader-keys
-      :states '(normal insert visual emacs)
-      :keymaps 'override
-      :prefix "SPC" ;; set leader
-      :global-prefix "M-SPC") ;; access leader in insert mode
+       :config
+       (general-evil-setup)
+        ;; set up 'SPC' as the global leader key
+     (general-create-definer w8ste/leader-keys
+       :states '(normal insert visual emacs)
+       :keymaps 'override
+       :prefix "SPC" ;; set leader
+       :global-prefix "M-SPC") ;; access leader in insert mode
 
-    (w8ste/leader-keys
-      "b" '(:ignore t :wk "buffer")
-      "bb" '(switch-to-buffer :wk "Switch buffer")
-      "bk" '(kill-this-buffer :wk "Kill this buffer")
-      "bn" '(next-buffer :wk "Next buffer")
-      "bp" '(previous-buffer :wk "Previous buffer")
-      "br" '(revert-buffer :wk "Reload buffer"))
-)
+     ;; buffer keybindings
+     (w8ste/leader-keys
+       "b" '(:ignore t :wk "buffer")
+       "bb" '(switch-to-buffer :wk "Switch buffer")
+       "bk" '(kill-this-buffer :wk "Kill this buffer")
+       "bi" '(ibuffer :wk "Ibuffer")
+       "bn" '(next-buffer :wk "Next buffer")
+       "bp" '(previous-buffer :wk "Previous buffer")
+       "br" '(revert-buffer :wk "Reload buffer"))
+
+ ;; quality of life keybindings
+ (w8ste/leader-keys
+  "f c" '((lambda () (interactive) (find-file "~/.config/emacs/config.org")) :wk "Edit emacs config")
+  "TAB TAB" '(comment-line :wk "Comment lines"))
+
+  ;; eval keybindings
+  (w8ste/leader-keys
+    "e" '(:ignore t :wk "Evaluate")    
+    "e b" '(eval-buffer :wk "Evaluate elisp in buffer")
+    "e d" '(eval-defun :wk "Evaluate defun containing or after point")
+    "e e" '(eval-expression :wk "Evaluate and elisp expression")
+    "e l" '(eval-last-sexp :wk "Evaluate elisp expression before point")
+    "e r" '(eval-region :wk "Evaluate elisp in region"))
+
+ (w8ste/leader-keys
+   "h" '(:ignore t :wk "Help")
+   "h f" '(describe-function :wk "Describe function")
+   "h v" '(describe-variable :wk "Describe variable"))
+
+ (w8ste/leader-keys
+  "t" '(:ignore t :wk "Toggle")
+  "t l" '(display-line-numbers-mode :wk "Toggle line numbers")
+  "t t" '(visual-line-mode :wk "Toggle truncated lines"))
+ )
+ ;; Setting RETURN key in org-mode to follow links
+   (setq org-return-follows-link  t)
 
 (set-face-attribute 'default nil
   :font "JetBrains Mono"
@@ -118,8 +144,7 @@
 ;; This is working in emacsclient but not emacs.
 ;; Your font must have an italic face available.
 (set-face-attribute 'font-lock-comment-face nil
-
-		    :slant 'italic)
+  :slant 'italic)
 (set-face-attribute 'font-lock-keyword-face nil
   :slant 'italic)
 
@@ -163,3 +188,7 @@
 	which-key-max-description-length 25
 	which-key-allow-imprecise-window-fit t
 	which-key-separator " → " ))
+
+(use-package elcord
+  :init
+  (elcord-mode))
