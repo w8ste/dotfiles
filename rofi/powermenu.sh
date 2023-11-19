913b7c8f928f1247692839a9d312c4063eaff094
Hannes Albert <hannes10042004@icloud.com>
2023-11-20 00:25:23 +0100

    Removed gaps and update picom

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/bash
 #
-chosen=$(printf "󰐥 Power Off\n Restart\n Log out" | rofi -dmenu -i -l 3)
+chosen=$(printf "󰐥 Power Off\n Restart\n Log out\n󰤄 Suspend" | rofi -dmenu -i -l 4)
 case "$chosen" in 
 	"󰐥 Power Off") shutdown now;;
 	" Restart") reboot ;;
     " Log out") i3-msg exit ;;
+    "󰤄 Suspend") systemctl suspend ;;
 	*) exit 1 ;;
 esac
